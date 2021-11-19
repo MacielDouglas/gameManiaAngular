6965995019a73f17bff80c4e57216c4a54dbc112
MacielDouglas <douglasmaciel.dm@gmail.com>
2021-11-19 18:16:57 -0300

    terminado layout Header, home, footer

---
+++ src/app/views/consoles/consoles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consoles',
+  templateUrl: './consoles.component.html',
+  styleUrls: ['./consoles.component.css']
+})
+export class ConsolesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
