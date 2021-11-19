0a60d4e32b992822a04897ad30a81533f57da572
MacielDouglas <douglasmaciel.dm@gmail.com>
2021-11-19 17:56:32 -0300

    terminado o layout header home footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,18 @@ import { HeaderComponent } from './views/header/header.component';
 import { HomeComponent } from './views/home/home.component';
 import { FooterComponent } from './views/footer/footer.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +28,17 @@ import { FooterComponent } from './views/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatListModule,
+    MatExpansionModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
